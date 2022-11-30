e9829409de233b8ec7cd5328cd1364f28f8424a4
jjoadevy116 <jjoaquinarlettaz@gmail.com>
2022-11-30 15:32:58 -0300

    Ejercicios en funcional

--- practicaParcial/funcional.lisp
+++ practicaParcial/funcional.lisp
@@ -76,7 +76,7 @@
 (defun listaMaxCant (lista)
   (linealizaSinSL (listaMax lista (cantMaxEl lista))))
 
-; Escriba una función/predicado que tome una lista L sin sublistas y número N, y devuelva la lista de las sumatorias de los elementos de L tomados de a N, es decir, el primer valor de la lista resultante será la sumatoria de los elemento de L desde el 1 al N, el segundo valor la sumatoria desde el 2 al N+1, y así sucesivamente. Si la cantidad de elementos de L es menor a N, se devuelve lista vacía. 
+; Escriba una funciï¿½n/predicado que tome una lista L sin sublistas y nï¿½mero N, y devuelva la lista de las sumatorias de los elementos de L tomados de a N, es decir, el primer valor de la lista resultante serï¿½ la sumatoria de los elemento de L desde el 1 al N, el segundo valor la sumatoria desde el 2 al N+1, y asï¿½ sucesivamente. Si la cantidad de elementos de L es menor a N, se devuelve lista vacï¿½a. 
 ; Ej: para L=(5 3 7 5 4 4 8 9) y N=3, el resultado es (15 15 16 13 16 21)
 
 (defun largo (lista)
@@ -101,7 +101,7 @@
    ((< (- (largo L) N) 0) nil)
    (T (cons (sumatoriaDesdeHasta L 1 N 1) (sumatoriaLista (rest L) N)))))
 
- ;10.Escriba una función/predicado que tome como entrada una lista con sublistas y devuelva la sublista cuya sumatoria (considerando sólo los elementos numéricos) sea la máxima.
+ ;10.Escriba una funciï¿½n/predicado que tome como entrada una lista con sublistas y devuelva la sublista cuya sumatoria (considerando sï¿½lo los elementos numï¿½ricos) sea la mï¿½xima.
 ;Ej: L=(5 (9 ((3 7 4)  5  (6 (15 3)) 4) 10) 9 1), el resultado es (9 10)
 
 (defun numerosDelNivel (lista)
@@ -134,7 +134,7 @@
 (defun listaMaxSum (lista)
   (devuelveListaMaxSum lista (maximaSumatoria lista)))
 
-; Escriba una función/predicado que tome una lista L donde todos sus elementos son sublistas de números ordenados de menor a mayor (sin sublistas) y devuelva la lista resultante de intercalar dichas sublistas, es decir, el resultado será una lista ordenada.
+; Escriba una funciï¿½n/predicado que tome una lista L donde todos sus elementos son sublistas de nï¿½meros ordenados de menor a mayor (sin sublistas) y devuelva la lista resultante de intercalar dichas sublistas, es decir, el resultado serï¿½ una lista ordenada.
 
 ; L=((4 9 13) (3) (1 2 5 8 8 11) () (7 24)) , el resultado es (1 2 3 4 5 7 8 8 9 11 13 24) 
 
@@ -178,9 +178,6 @@
 (defun ordenarListas (listaConSL)
   (menorAmayor (linealizaLista listaConSL)))
 
-
-
-
 ; No se utilizo (ordena pero solo de a 'pares')
 (defun ordenar (listaLineal ordenamiento)
   (cond
@@ -191,7 +188,60 @@
 ; Input: ordenar '(5 3 2 0 1 4) (lambda (x y) (> x y)) => NO LO HARIA BIEN
 ; Input: ordenar '(5 2 3 0 1) (lambda (x y) (> x y)) => (5 3 2 1 0)
 
+; Escriba una funcion (predicado) que tome como entrada una lista L (sin sublistas) y una lista M (que puede contener sublistas), y  devuelva una lista con N sublistas, donde N es la cantidad de elementos de L. Cada sublista debe contener todas las posiciones del i-esimo elemento de L en M (como si M fuese lineal. Ademas, si el elemento no existe se pondra 0).
 
+;Ejemplo: L=(6 3 2 4 8) M=(2 (5 4 7 7) 5 (3 (4 9) 10) 6 (5 7) 4 9 2)
+;Resultado: ((11) (7) (1 16) (3 8 14) (0))
+
+; Funcion auxiliar que devuelva las posiciones de un elemento en una lista y devuelve nil si no esta en la lista.
+(defun posicionesDelElemento (elemento lista pos)
+  (cond
+   ((null lista) nil)
+   ((= elemento (first lista)) (cons pos (posicionesDelElemento elemento (rest lista) (+ 1 pos))))
+   (T (posicionesDelElemento elemento (rest lista) (+ 1 pos)))))
+
+; Funcion que devuelve las posiciones del elemento en una lista, devuelve una lista con (0) si la auxiliar devuelve nil (no esta el elemento en la lista)
+(defun posDelElem (elemento lista)
+  (cond
+   ((not (posicionesDelElemento elemento lista 1)) (cons 0 '()))
+   (T (posicionesDelElemento elemento lista 1))))
+
+(defun linealizar (L)
+  (cond
+   ((null L) L)
+   ((listp (first L)) (append (linealizar (first L)) (linealizar (rest L))))
+   (T (cons (first L) (linealizar (rest L))))))
+
+; La lista debe estar linealizada para que funcione (que esten todos los elementos en el mismo nivel)
+(defun devolverPosicionesEnListaLineal (L M)
+  (cond
+   ((null L) L)
+   (T (cons (posDelElem (first L) M) (devolverPosicionesEnListaLineal (rest L) M)))))
+
+(defun devolverPosiciones (L M)
+  (devolverPosicionesEnListaLineal L (linealizar M)))
+
+; Devolver la posicion de un elemento de una lista L segun los elementos de una lista M, si no esta, se omite. 
+; Si el elemento no esta en un subnivel y hay sublistas se busca dentro de estas.
+
+(defun encontrarPosEl (E L P) ; P es la pos y debe empezar en 1
+  (cond
+   ((null L) 0)
+   ((and (not (listp (first L))) (= (first L) E)) P)
+   (T (encontrarPosEl E (rest L) (+ 1 P)))))
+
+(defun devolverPosEl (E L)
+  (cond
+   ((null L) 0)
+   ((/= 0 (encontrarPosEl E L 1)) (encontrarPosEl E L 1))
+   ((listp (first L)) (devolverPosEl E (first L)))
+   (T (devolverPosEl E (rest L)))))
+
+(defun posicionesDeElementos (L M)
+  (cond
+   ((null M) M)
+   ((/= 0 (devolverPosEl (first M) L)) (cons (devolverPosEl (first M) L) (posicionesDeElementos L (rest M))))
+   (T (posicionesDeElementos L (rest M)))))
 
 
 
